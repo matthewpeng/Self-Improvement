b24e0499a8eec5f9b19cc4a6e0bd56a011edb421
matthew <68773479@qq.com>
2019-03-09 16:34:53 +0800

    Initial commit

---
+++ src/com/practice/leetcode/Easy.java
@@ -0,0 +1,39 @@
+package com.practice.leetcode;
+
+/**
+ * @author: ex_pengyw
+ * @Date: 2019/3/9 16:12
+ * @Description:
+ */
+public class Easy {
+
+    public static void main(String[] args) {
+
+        int[] ints = twoSum(new int[]{3, 3}, 6);
+        System.out.println("answer : " + ints.toString());
+
+    }
+
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * 示例:
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                int result = nums[i] + nums[j];
+                if (result == target && i != j) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+}

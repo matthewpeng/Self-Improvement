cadb67637d830ce97eb187b81d35687d48d2e198
matthew <68773479@qq.com>
2019-03-13 16:12:24 +0800

    9. 回文数

--- src/com/practice/leetcode/Easy.java
+++ src/com/practice/leetcode/Easy.java
@@ -10,12 +10,42 @@ import java.util.ArrayList;
 public class Easy {
 
     public static void main(String[] args) {
+        //1. 两数之和
+        //int[] ints = twoSum(new int[]{3, 3}, 6);
+        //System.out.println("answer : " + ints.toString());
 
-//        int[] ints = twoSum(new int[]{3, 3}, 6);
-//        System.out.println("answer : " + ints.toString());
+        //7. 整数反转
+        //System.out.println(reverse(1534236469));
 
-        System.out.println(reverse(1534236469));
 
+        //9. 回文数
+        System.out.println(isPalindrome(-121));
+    }
+
+    /**
+     * 9. 回文数
+     * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     * 示例 1:
+     * 输入: 121
+     * 输出: true
+     * 示例 2:
+     * 输入: -121
+     * 输出: false
+     * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     * 示例 3:
+     * 输入: 10
+     * 输出: false
+     * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+     */
+    public static boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - 1-i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**

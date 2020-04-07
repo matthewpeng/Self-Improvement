42b142670cb883c4aa14c33f3850e617378396f0
matthew <68773479@qq.com>
2020-04-07 21:07:42 +0800

    27. 移除元素

--- src/com/practice/leetcode/Easy.java
+++ src/com/practice/leetcode/Easy.java
@@ -42,10 +42,28 @@ public class Easy {
 //        System.out.println(isValid("{[]}"));
 
         //26. 删除排序数组中的重复项
-        System.out.println(removeDuplicates(new int[]{1, 1, 2}));
-        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
+//        System.out.println(removeDuplicates(new int[]{1, 1, 2}));
+//        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
 
 
+        //27. 移除元素
+        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+        System.out.println(removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
+    }
+
+    /**
+     * 27. 移除元素
+     */
+    public static int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return j;
     }
 
 
@@ -64,7 +82,7 @@ public class Easy {
 
         }
         System.out.println(Arrays.toString(nums));
-        return j+1;
+        return j + 1;
     }
 
 
